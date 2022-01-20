229a918b357a5f1f8225fcb15c93c48416f3f56e
firas-debich <debichf02@gmail.com>
2022-01-20 18:26:53 +0100

    add to block chain

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -7,4 +7,27 @@ contract Transactions {
     uint256 transactionsCounter;
 
 
+    event Transfer(address from , address receiver, uint amount,string message , uint256 timestamp,string keyword);
+
+    struct TransferStruct {
+        address sender;
+        address receiver; 
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;  
+    }
+ TransferStruct[] transactions;
+
+ function addToBlockchain (address payable receiver, uint amount, string memory message , string memory keyword ) public{
+    transactionsCounter+= 1;
+    transactions.push(TransferStruct(msg.sender,receiver, amount ,message,block.timestamp,keyword));
+ }   
+ function getAllTansactions () public view returns (TransferStruct[] memory){
+     
+ }  
+  function getTransactionsCount () public view returns (uint256){
+     
+ }   
+
 }
