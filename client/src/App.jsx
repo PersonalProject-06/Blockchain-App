1b77111f07ca020a0b3308fb15bf5c2ef7ae6242
firas-debich <debichf02@gmail.com>
2022-01-20 13:50:49 +0100

    project structure done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import {Navbar,Transactions,Welcome,Footer,Loader,Services} from "./components/index"
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
     </div>
   );
 };
